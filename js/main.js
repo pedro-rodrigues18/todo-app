44ae8c51ab200b23dfd803bfec2fe1e9a669f78d
Pedro Rodrigues <phrp.blog@gmail.com>
2021-04-20 15:47:51 -0300

    Função remover tarefa e outras pequenas modificações

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const inputEle = document.getElementById('enter-tarefa');
 let lista = document.getElementById('lista').innerHTML;
 let numeroDeTarefas = document.getElementById('numero-de-tarefas').innerHTML;
+let remove = document.querySelector('#remover-tarefa');
 
 let tarefa;
 let contador = 0;
@@ -9,28 +10,58 @@ let listaDeTarefas = [];
 
 inputEle.addEventListener('keyup', function (e) {
   let key = e.which || e.keyCode;
+
   if (key == 13) {
     tarefa = this.value;
-    listaDeTarefas.push(tarefa);
-    adicionarTarefa(tarefa);
-    contador += 1;
-    modificarContador(contador);
+
+    if (tarefa != '') {
+      contador++;
+      listaDeTarefas.push(tarefa);
+      adicionarTarefa(tarefa, contador);
+      modificarContador(contador);
+
+    } else {
+      alert("Erro: não é possível criar tarefa vazia.");
+    }
+
     document.getElementById('enter-tarefa').value = null;
   }
 });
 
-function adicionarTarefa(tarefa) {
+function adicionarTarefa(tarefa, contador) {
   lista += `
-      <li class="tarefa">
+    <li class="tarefa" id="tarefa-${contador - 1}">
+      <a href="#" class="link-tarefa">
         <span class="check-tarefa">
           
         </span>
         <div class="tarefa-texto">${tarefa}</div>
-      </li>
+      </a>
+      <a href="#" class="remover-tarefa" id="${contador - 1}" onclick="removerTarefa(id);">
+        <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
+      </a>
+    </li>
     `;
 
   document.getElementById('lista').innerHTML = lista;
 }
+
+function removerTarefa(id) {
+
+  // Ainda está sendo implementado
+  // Foi identificado falhas ao tentar adicionar novas tarefas após uma remoção.
+
+  let li = document.getElementById(id).parentNode;
+
+  li.remove();
+
+  console.log(li);
+
+  contador--;
+  modificarContador(contador);
+
+}
+
 
 function modificarContador(contador) {
 
