84dc2e662f63560bdf28af08ae45de57a1be4b87
Pedro Rodrigues <phrp.blog@gmail.com>
2021-04-22 19:06:58 -0300

    Modificações na função de inserir tarefa

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 
-const inputEle = document.getElementById('enter-tarefa');
-let lista = document.getElementById('lista').innerHTML;
+const inserirTarefa = document.getElementById('enter-tarefa');
+let listaFinal = document.getElementById('lista');
 let numeroDeTarefas = document.getElementById('numero-de-tarefas').innerHTML;
-let remove = document.querySelector('#remover-tarefa');
 
-let tarefa;
 let contador = 0;
+let li;
+let tarefa;
 let listaDeTarefas = [];
 
-inputEle.addEventListener('keyup', function (e) {
+inserirTarefa.addEventListener('keyup', function (e) {
+
   let key = e.which || e.keyCode;
 
   if (key == 13) {
     tarefa = this.value;
 
     if (tarefa != '') {
+
       contador++;
-      listaDeTarefas.push(tarefa);
-      adicionarTarefa(tarefa, contador);
-      modificarContador(contador);
+
+      li = `
+        <li class="tarefa" id="tarefa-${contador - 1}">
+          <a href="#" class="link-tarefa" id="check-${contador - 1}" onclick="marcarTarefa(id);">
+            <span class="check-tarefa"></span>
+            <div class="tarefa-texto">${tarefa}</div>
+          </a>
+          <a href="#" class="remover-tarefa" id="remover-${contador - 1}" onclick="removerTarefa(id);">
+            <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
+          </a>
+        </li>
+      `;
+
+      listaDeTarefas.push(li);
+
+      atualizarLista(listaDeTarefas);
 
     } else {
       alert("Erro: não é possível criar tarefa vazia.");
@@ -28,46 +43,40 @@ inputEle.addEventListener('keyup', function (e) {
   }
 });
 
-function adicionarTarefa(tarefa, contador) {
-  lista += `
-    <li class="tarefa" id="tarefa-${contador - 1}">
-      <a href="#" class="link-tarefa">
-        <span class="check-tarefa">
-          
-        </span>
-        <div class="tarefa-texto">${tarefa}</div>
-      </a>
-      <a href="#" class="remover-tarefa" id="${contador - 1}" onclick="removerTarefa(id);">
-        <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
-      </a>
-    </li>
-    `;
-
-  document.getElementById('lista').innerHTML = lista;
-}
-
 function removerTarefa(id) {
 
-  // Ainda está sendo implementado
-  // Foi identificado falhas ao tentar adicionar novas tarefas após uma remoção.
+}
 
-  let li = document.getElementById(id).parentNode;
+function atualizarLista(listaDeTarefas) {
 
-  li.remove();
+  console.log(listaDeTarefas.length);
 
-  console.log(li);
+}
+
+function marcarTarefa(id) {
+  event.preventDefault();
+
+  let linkTarefa = document.getElementById(id);
+  let itensLinkTarefa = linkTarefa.children;
+
+  //mudando a cor de fundo do span check e adicionando svg check
+  itensLinkTarefa[0].style.background = 'linear-gradient(#57ddff, #c058f3)';
+  itensLinkTarefa[0].innerHTML = `<img src="./images/icon-check.svg" alt="check">`;
+
+  // mudando cor do texto e adcionando linha horizontal sobre ele
+  itensLinkTarefa[1].style.color = '#777a92';
+  itensLinkTarefa[1].style.textDecoration = 'line-through';
 
   contador--;
   modificarContador(contador);
 
 }
 
-
 function modificarContador(contador) {
 
-  numeroDeTarefas = `
-    <p id="numero-de-tarefas">${contador} items restantes</p>
-  `;
+  // numeroDeTarefas = `
+  //   <p id="numero-de-tarefas">${contador} items restantes</p>
+  // `;
 
-  document.getElementById('numero-de-tarefas').innerHTML = numeroDeTarefas;
+  // document.getElementById('numero-de-tarefas').innerHTML = numeroDeTarefas;
 }
