aa3f2d34e2ef2c7e6f2db1ae4a398e2584783ffc
Pedro Rodrigues <phrp.blog@gmail.com>
2021-04-27 08:50:36 -0300

    Correções para manter Todo marcado ao adicionar ou remover tarefa

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ const inserirTarefa = document.getElementById('enter-tarefa');
 const lista = document.getElementById('lista');
 const numeroDeTarefas = document.getElementById('numero-de-tarefas');
 
+class Todo {
+  tarefa;
+  check = false;
+}
 let contador = 0;
 let conteudo;
-let listaDeTarefas = [];
+let listaDeTodo = [];
 
 inserirTarefa.addEventListener('keyup', function (e) {
 
@@ -15,9 +19,12 @@ inserirTarefa.addEventListener('keyup', function (e) {
 
     if (conteudo != '') {
 
-      listaDeTarefas.push(conteudo);
+      todo = new Todo;
+      todo.tarefa = conteudo;
 
-      renderizarLista(listaDeTarefas);
+      listaDeTodo.push(todo);
+
+      renderizarLista(listaDeTodo);
 
       contador++;
 
@@ -36,9 +43,9 @@ function removerTarefa(id) {
 
   tarefa = document.getElementById(id).parentNode;
 
-  listaDeTarefas.splice(id, 1);
+  listaDeTodo.splice(id, 1);
 
-  renderizarLista(listaDeTarefas);
+  renderizarLista(listaDeTodo);
 
   //console.log(tarefa.children[0]);
   if (!tarefa.children[0].classList.contains('check-tarefa-concluida')) {
@@ -48,20 +55,35 @@ function removerTarefa(id) {
 
 }
 
-function renderizarLista(listaDeTarefas) {
+function renderizarLista(listaDeTodo) {
   lista.innerHTML = '';
-  for (let i = 0; i < listaDeTarefas.length; i++) {
-    lista.innerHTML += `
-      <li class="tarefa">
-        <a href="#" class="link-tarefa" id="check-${i}" onclick="marcarOuDesmarcarTarefa(id);">
-          <span class="check-tarefa"></span>
-          <div class="tarefa-texto">${listaDeTarefas[i]}</div>
-        </a>
-        <a href="#" class="remover-tarefa" id="${i}" onclick="removerTarefa(id);">
-          <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
-        </a>
-      </li>
-    `;
+  for (let i = 0; i < listaDeTodo.length; i++) {
+    if (listaDeTodo[i].check == false) {
+      lista.innerHTML += `
+        <li class="tarefa">
+          <a href="#" class="link-tarefa" id="check-${i}" onclick="marcarOuDesmarcarTarefa(id);">
+            <span class="check-tarefa"></span>
+            <div class="tarefa-texto">${listaDeTodo[i].tarefa}</div>
+          </a>
+          <a href="#" class="remover-tarefa" id="${i}" onclick="removerTarefa(id);">
+            <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
+          </a>
+        </li>
+      `;
+    }
+    else {
+      lista.innerHTML += `
+        <li class="tarefa">
+          <a href="#" class="link-tarefa check-tarefa-concluida" id="check-${i}" onclick="marcarOuDesmarcarTarefa(id);">
+            <span class="check-tarefa"><img src="./images/icon-check.svg" alt="check"></span>
+            <div class="tarefa-texto">${listaDeTodo[i].tarefa}</div>
+          </a>
+          <a href="#" class="remover-tarefa" id="${i}" onclick="removerTarefa(id);">
+            <img src="./images/icon-cross.svg" alt="Remover Tarefa" class="remover-tarefa">
+          </a>
+        </li>
+      `;
+    }
   }
 }
 
@@ -71,7 +93,13 @@ function marcarOuDesmarcarTarefa(id) {
   let linkTarefa = document.getElementById(id);
   let itensLinkTarefa = linkTarefa.children;
 
+  idNumero = id.split("").filter(n => (Number(n) || n == 0)).join("");
+
+  console.log(idNumero);
+
   if (!linkTarefa.classList.contains('check-tarefa-concluida')) {
+
+    listaDeTodo[idNumero].check = true;
 
     linkTarefa.classList.add('check-tarefa-concluida');
     itensLinkTarefa[0].innerHTML = `<img src="./images/icon-check.svg" alt="check">`;
@@ -80,6 +108,8 @@ function marcarOuDesmarcarTarefa(id) {
     modificarContador(contador);
 
   } else {
+    listaDeTodo[idNumero].check = false;
+
     linkTarefa.classList.remove('check-tarefa-concluida');
     itensLinkTarefa[0].innerHTML = '';
 
